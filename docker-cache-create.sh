de3ac918907fb550dfbac2def460a9512b22996a
Jan Schumann <jschumann.ext@juris.de>
2023-08-05 01:21:50 +0200

    push pr results

--- docker-cache-create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DOCKER_TAG=${DOCKER_METADATA_OUTPUT_TAGS##\n*}
-mkdir -p "$GITHUB_WORKSPACE/docker"
-docker save -o "$GITHUB_WORKSPACE/docker/cache.tar" ${DOCKER_TAG}
--- docker-cache-restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ -f "$GITHUB_WORKSPACE/docker/cache.tar" ]; then
-  docker load -i "$GITHUB_WORKSPACE/docker/cache.tar"
-fi
